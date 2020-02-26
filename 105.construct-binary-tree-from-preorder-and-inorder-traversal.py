8692cf1ffb9f205aa53a2e44dcd10c732f05f949
chanyinwa <chaincain@gmail.com>
2020-02-26 13:25:25 -0800

    105

---
+++ 105.construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -0,0 +1,63 @@
+#
+# @lc app=leetcode id=105 lang=python3
+#
+# [105] Construct Binary Tree from Preorder and Inorder Traversal
+#
+# https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
+#
+# algorithms
+# Medium (45.38%)
+# Likes:    2609
+# Dislikes: 75
+# Total Accepted:    303.3K
+# Total Submissions: 663.4K
+# Testcase Example:  '[3,9,20,15,7]\n[9,3,15,20,7]'
+#
+# Given preorder and inorder traversal of a tree, construct the binary tree.
+# 
+# Note:
+# You may assume that duplicates do not exist in the tree.
+# 
+# For example, given
+# 
+# 
+# preorder = [3,9,20,15,7]
+# inorder = [9,3,15,20,7]
+# 
+# Return the following binary tree:
+# 
+# 
+# ⁠   3
+# ⁠  / \
+# ⁠ 9  20
+# ⁠   /  \
+# ⁠  15   7
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        if not preorder or not inorder:
+            return None
+
+
+        root = TreeNode(preorder[0])
+        root_index = inorder.index(preorder[0])
+
+        root.left = self.buildTree(preorder[1:root_index+1], inorder[:root_index])
+        root.right = self.buildTree(preorder[root_index+1:], inorder[root_index+1:])
+
+
+
+
+
+        return root 
+# @lc code=end
