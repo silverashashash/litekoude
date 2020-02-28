570df1a6a9514397be751e0e3ccf56d910c01e64
chanyinwa <chaincain@gmail.com>
2020-02-28 10:03:03 -0800

    300

---
+++ 300.longest-increasing-subsequence.py
@@ -0,0 +1,54 @@
+#
+# @lc app=leetcode id=300 lang=python3
+#
+# [300] Longest Increasing Subsequence
+#
+# https://leetcode.com/problems/longest-increasing-subsequence/description/
+#
+# algorithms
+# Medium (41.86%)
+# Likes:    3724
+# Dislikes: 84
+# Total Accepted:    315.4K
+# Total Submissions: 752.7K
+# Testcase Example:  '[10,9,2,5,3,7,101,18]'
+#
+# Given an unsorted array of integers, find the length of longest increasing
+# subsequence.
+# 
+# Example:
+# 
+# 
+# Input: [10,9,2,5,3,7,101,18]
+# Output: 4 
+# Explanation: The longest increasing subsequence is [2,3,7,101], therefore the
+# length is 4. 
+# 
+# Note: 
+# 
+# 
+# There may be more than one LIS combination, it is only necessary for you to
+# return the length.
+# Your algorithm should run in O(n^2) complexity.
+# 
+# 
+# Follow up: Could you improve it to O(n log n) time complexity?
+# 
+#
+
+# @lc code=start
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        dp = [0 for _ in range(len(nums))]
+        
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j])
+            dp[i] += 1
+
+        return max(dp)
+        
+# @lc code=end
